d1c6a2c067b476bb97640cde153a6e833db856b3
Matheus Lovatti <matheusamorimlovatti@gmail.com>
2026-04-14 20:58:48 -0300

    feat: add a new transition page animation to Projects Page with a fade-in effect. fix: resolve an issue in fade-out animation in Home Page.

--- script/transition-out.js
+++ script/transition-out.js
@@ -1,40 +1,40 @@
-let isAnimating = false;
+document.addEventListener("DOMContentLoaded", () => {
 
-document.querySelectorAll(".page-transition").forEach(link => {
-  link.addEventListener("click", function (e) {
-  e.preventDefault();
+  let isAnimating = false;
 
-    if (isAnimating) return;
-    isAnimating = true;
+  document.querySelectorAll(".page-transition").forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
 
-    const targetUrl = this.getAttribute("href");
+      if (isAnimating) return;
+      isAnimating = true;
 
-    const tl = gsap.timeline({
-      onComplete: () => {
-        setTimeout(() => {
+      const targetUrl = this.getAttribute("href");
+
+      const tl = gsap.timeline({
+        onComplete: () => {
           window.location.href = targetUrl;
-        }, 100);
-      }
-    });
+        }
+      });
 
-    // 1️⃣ Fade geral da página
-    tl.to(".transition-item:not(.transition-zoom-out)", {
-      opacity: 0,
-      y: 30,
-      filter: "blur(8px)",
-      duration: 0.5,
-      stagger: 0.06,
-      ease: "power2.in"
-    });
+      tl.to(".transition-item:not(.transition-zoom-out)", {
+        opacity: 0,
+        y: 30,
+        filter: "blur(8px)",
+        duration: 0.5,
+        stagger: 0.06,
+        ease: "power2.in"
+      });
 
-    // 2️⃣ Card de contato (zoom + fade progressivo + blur)
-    tl.to(".transition-zoom-out", {
-      scale: 0.5,
-      opacity: 0,
-      filter: "blur(14px)",
-      y: 20,
-      duration: 1,
-      ease: "power4.inOut"
-    }, "-=0.2");
+      tl.to(".transition-zoom-out", {
+        scale: 0.5,
+        opacity: 0,
+        filter: "blur(14px)",
+        y: 20,
+        duration: 1,
+        ease: "power4.inOut"
+      }, "-=0.2");
+    });
   });
+
 });
